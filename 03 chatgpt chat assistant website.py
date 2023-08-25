b92f44d02b08432a8227bd32839286c295c23cb7
Saira Arif <sairaarif364@gmail.com>
2023-08-25 15:05:02 +0500

    first commit

---
+++ 03 chatgpt chat assistant website.py
@@ -0,0 +1,20 @@
+import openai
+import gradio
+
+openai.api_key = "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"
+
+messages = [{"role": "system", "content": "You are a financial experts that specializes in real estate investment and negotiation"}]
+
+def CustomChatGPT(user_input):
+    messages.append({"role": "user", "content": user_input})
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = messages
+    )
+    ChatGPT_reply = response["choices"][0]["message"]["content"]
+    messages.append({"role": "assistant", "content": ChatGPT_reply})
+    return ChatGPT_reply
+
+demo = gradio.Interface(fn=CustomChatGPT, inputs = "text", outputs = "text", title = "Real Estate")
+
+demo.launch(share=True)
